d7b6e91f6a85d22fdcd4af23471275fdb76e00ef
SushmaTripathi28 <128452108+SushmaTripathi28@users.noreply.github.com>
2024-06-27 01:42:29 +0530

    Update app.py

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -3,15 +3,27 @@ from app import APP
 
 
 class Tests(unittest.TestCase):
+    """
+    Basic tests for the application
+    """
 
     def setUp(self):
+        """
+        Create a test client for the app
+        """
         self.app = APP.test_client()
 
     def test_200(self):
+        """
+        test_200: a request for / shall return 200 OK
+        """
         res = self.app.get('/')
         self.assertEqual(res.status, '200 OK')
 
     def test_404(self):
+        """
+        test_404: a request for /null shall return 404 NOT FOUND
+        """
         res = self.app.get('/null')
         self.assertEqual(res.status, '404 NOT FOUND')
 
